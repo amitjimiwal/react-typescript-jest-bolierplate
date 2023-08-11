7992bbc58941bb3604d694d4fddea09fd3849a21
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-11 10:53:27 +0530

    tailwind integrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from "react";
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React + TS + Testing Template</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="bg-black h-screen w-full flex justify-center items-center">
+      <h1 className="text-3xl text-white">Vite + React + TS + Testing Template</h1>
+    </div>
   );
 }
 
